ab9dffef00275a14d92106dbe504e7a059518382
Adil Shakeel <90831887+itsAdil45@users.noreply.github.com>
2024-07-24 17:12:24 +0500

    integrate all games, add svelte routing

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 	import Keno from './Keno.svelte';
 	import Home from './Home.svelte';
     import MineSweeper from './mine-sweeper.svelte';
+	import HILO from "./HILO.svelte"
 
 </script>
 
@@ -12,10 +13,14 @@
 		<Link to="/">Home</Link>
 		<Link to="/Minesweeper">Minesweeper</Link>
 		<Link to="/Keno">Keno</Link>
+		<Link to="/HILO">HILO</Link>
+
 	</header>
 <main>
 	<Route primary={false} path="/" ><Home/></Route>
 	<Route primary={false} path="/Minesweeper" ><MineSweeper/></Route>
 	<Route primary={false} path="/Keno" ><Keno/></Route>
+	<Route primary={false} path="/HILO" ><HILO/></Route>
+
 </main>
 </Router>
